80b4a20a56fb75cb033bb7852df6c5b237e1cddd
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-10-13 13:34:04 +0200

    addMoney
    sss

---
+++ Prototyping 5 - Money/Assets/Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public int bank;
+    public int addmoney;
+    public float moneyTimer;
+    public bool loopTimer;
+
+	void Start ()
+    {
+        loopTimer = true;
+	}
+	
+	void Update ()
+    {
+        if (loopTimer == true)
+        {
+            StartCoroutine(AddmoneyTimer());
+            loopTimer = false;
+        }
+	}
+
+    IEnumerator AddmoneyTimer()
+    {
+        yield return new WaitForSeconds(moneyTimer);
+        bank += addmoney;
+        Debug.Log("hoi");
+        loopTimer = true;
+    }
+
+}
