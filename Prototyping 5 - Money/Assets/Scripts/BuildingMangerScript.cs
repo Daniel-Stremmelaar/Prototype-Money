b3d1ed0293ce818342f84e2fd5deadc6e629f4ea
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-10-16 17:19:57 +0200

    Prototype V1
    Version 1 of prototype done push

--- Prototyping 5 - Money/Assets/Scripts/BuildingMangerScript.cs
+++ Prototyping 5 - Money/Assets/Scripts/BuildingMangerScript.cs
@@ -1,16 +1,127 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingMangerScript : MonoBehaviour {
 
+    public int workers;
+    public float workerMultiplier;
+    public float timer;
+    public float timerReset;
+    public int value;
+    public int minValue;
+    public int maxValue;
+    public int change;
+    public int minChange;
+    public int maxChange;
+    public bool cursus;
+    public bool toilet;
+    public GameObject moneyManager;
+    public Button neg;
+    public Button pos;
+    public Text wokerText;
+    public Image indicator;
+    public Color red;
+    public Color green;
+    public Color black;
+    public bool reset;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        red = new Color(255, 0, 0);
+        green = new Color(0, 255, 0);
+        value = Random.Range(minValue, maxValue);
+        neg.onClick.AddListener(delegate { ChangeWorkers(-1); });
+        pos.onClick.AddListener(delegate { ChangeWorkers(1); });
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if(workers < 1)
+        {
+            workerMultiplier = 0;
+        }
+        else
+        {
+            workerMultiplier = 1;
+            workerMultiplier += (workers - 1) / 100;
+        }
+
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            if(reset == true)
+            {
+                minValue = 0;
+                maxValue = 2;
+                indicator.GetComponent<Image>().color = green;
+                reset = false;
+            }
+            Revenue(value);
+            value = Random.Range(minValue, maxValue);
+            if(value > 0)
+            {
+                indicator.GetComponent<Image>().color = green;
+            }
+            else
+            {
+                indicator.GetComponent<Image>().color = red;
+            }
+            change = Random.Range(minChange, maxChange);
+            minValue += change;
+            maxValue += change;
+            if(minValue < -7 && maxValue <= 0)
+            {
+                indicator.GetComponent<Image>().color = black;
+                reset = true;
+                timer = 20;
+            }
+            else
+            {
+                timer = timerReset;
+            }
+        }
 	}
+
+    void Revenue(float f)
+    {
+        f *= workerMultiplier;
+        if(cursus == true)
+        {
+            f *= 1.20f;
+        }
+        if(toilet == true)
+        {
+            f *= 1.10f;
+        }
+        moneyManager.GetComponent<MoneyManager>().ChangeMoney(f);
+    }
+
+    void ChangeWorkers(int i)
+    {
+        if(i == 1)
+        {
+            if (moneyManager.GetComponent<MoneyManager>().reserves < 1)
+            {
+                print("not enough workers");
+            }
+            else
+            {
+                moneyManager.GetComponent<MoneyManager>().ChangeReserves(-i);
+                workers += i;
+                wokerText.text = workers.ToString();
+            }
+        }
+        if(i == -1)
+        {
+            if(workers > 0)
+            {
+                workers += i;
+                wokerText.text = workers.ToString();
+                moneyManager.GetComponent<MoneyManager>().ChangeReserves(-i);
+            }
+        }
+    }
 }
---
+++ Prototyping 5 - Money/Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    public int bank;
+    public Text bankText;
+    public int addmoney;
+    public float moneyTimer;
+    public bool loopTimer;
+    public int reserves;
+    public Text reserveText;
+    public int cost;
+    public Text costText;
+
+    public Button hire;
+    public Button coffeeButton;
+    public Button sleepButton;
+    public Button cursusButton;
+    public Button toiletButton;
+
+    public bool coffee;
+    public bool sleep;
+
+    public List<BuildingMangerScript> Buildings = new List<BuildingMangerScript>();
+
+	void Start ()
+    {
+        addmoney = 100;
+        bank = 0;
+        reserves = 3;
+        loopTimer = true;
+        hire.onClick.AddListener(Hire);
+        coffeeButton.onClick.AddListener(delegate { GainUpgrade(1); });
+        sleepButton.onClick.AddListener(delegate { GainUpgrade(2); });
+        cursusButton.onClick.AddListener(delegate { GainUpgrade(3); });
+        toiletButton.onClick.AddListener(delegate { GainUpgrade(4); });
+        reserveText.text = "Reserves: " + reserves.ToString();
+        bankText.text = "Bank: " + bank.ToString();
+        costText.text = "Hire - $" + cost.ToString();
+    }
+	
+	void Update ()
+    {
+        if (loopTimer == true)
+        {
+            StartCoroutine(AddmoneyTimer(moneyTimer));
+            loopTimer = false;
+        }
+	}
+
+    IEnumerator AddmoneyTimer(float timer)
+    {
+        if (coffee == true)
+        {
+            timer /= 2;
+        }
+        if (sleep == true)
+        {
+            timer *= 1.5f;
+        }
+        yield return new WaitForSeconds(timer);
+        ChangeMoney(reserves);
+        Debug.Log("hoi");
+        loopTimer = true;
+    }
+
+    public void ChangeMoney(float gain)
+    {
+        if(gain > 0)
+        {
+            if(sleep == true)
+            {
+                gain *= 2;
+            }
+        }
+        bank += Mathf.FloorToInt(gain); ;
+        bankText.text = "Bank: " + bank.ToString();
+    }
+
+    public void ChangeReserves(int i)
+    {
+        reserves += i;
+        reserveText.text = "Reserves: " + reserves.ToString();
+    }
+
+    void GainUpgrade(int i)
+    {
+        if(i == 1)
+        {
+            if(bank > 50)
+            {
+                coffee = true;
+                coffeeButton.gameObject.SetActive(false);
+                bank -= 50;
+            }
+        }
+        if(i == 2)
+        {
+            if(bank > 25)
+            {
+                sleep = true;
+                sleepButton.gameObject.SetActive(false);
+                bank -= 25;
+            }
+        }
+        if(i == 3)
+        {
+            if(bank > 50)
+            {
+                foreach (BuildingMangerScript b in Buildings)
+                {
+                    b.cursus = true;
+                }
+                cursusButton.gameObject.SetActive(false);
+                bank -= 50;
+            }
+        }
+        if(i == 4)
+        {
+            if(bank > 75)
+            {
+                foreach (BuildingMangerScript b in Buildings)
+                {
+                    b.toilet = true;
+                }
+                toiletButton.gameObject.SetActive(false);
+                bank -= 75;
+            }
+        }
+    }
+
+    void Hire()
+    {
+        if(bank < cost)
+        {
+            print("Not enough cash");
+        }
+        else
+        {
+            reserves += 1;
+            reserveText.text = "Reserves: " + reserves.ToString();
+            ChangeMoney(-cost);
+            cost *= 3;
+            costText.text = "Hire - $" + cost.ToString();
+        }
+    }
+
+}
